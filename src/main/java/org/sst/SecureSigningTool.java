9d153de7f8351483e561a5aab141ffdf528abe4c
Luis Pereira <LuisMPereira23@gmail.com>
2023-03-17 17:17:06 +0000

    Changed listKey view, added sign Ed25519

--- src/main/java/org/sst/SecureSigningTool.java
+++ src/main/java/org/sst/SecureSigningTool.java
@@ -1,25 +1,24 @@
 package org.sst;
 
+import dnl.utils.text.table.TextTable;
 import org.apache.commons.cli.*;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.bouncycastle.asn1.ASN1ObjectIdentifier;
 import org.bouncycastle.asn1.ASN1InputStream;
+import org.bouncycastle.asn1.ASN1ObjectIdentifier;
 import org.bouncycastle.asn1.ASN1Primitive;
 import org.bouncycastle.asn1.ASN1String;
-import org.bouncycastle.asn1.edec.EdECObjectIdentifiers;
 import org.bouncycastle.asn1.x9.ECNamedCurveTable;
-import org.bouncycastle.asn1.x9.X9ECParameters;
-import org.bouncycastle.jcajce.provider.asymmetric.util.EC5Util;
-import org.bouncycastle.operator.DefaultAlgorithmNameFinder;
 import org.pkcs11.jacknji11.*;
 
 import java.io.IOException;
-import java.math.BigInteger;
+import java.lang.instrument.Instrumentation;
 import java.nio.charset.StandardCharsets;
-import java.security.spec.ECParameterSpec;
-import java.util.HashMap;
-import java.util.Map;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
 
 public class SecureSigningTool {
     private static final Log log = LogFactory.getLog(SecureSigningTool.class);
@@ -27,11 +26,12 @@ public class SecureSigningTool {
     private static final Option HSM_SLOT = new Option("hsm_slot", true, "HSM Slot");
     private static final Option P11_LIB = new Option("p11library", true, "Path to PKCS#11 library .so file");
     private static final Option HSM_SLOT_PWD = new Option("hsm_slot_pwd", true, "HSM slot password");
+    private static final Option KEY_REF = new Option("key_ref", true, "Key Reference");
+    private static final Option HASH_ALGO = new Option("hash_algorithm", true, "Hash algorithm");
+    private static final Option PATH = new Option("path", true, "Path of data to be signed");
 
     private static byte[] USER_PIN;
     private static long INITSLOT;
-
-    private static HashMap<Long,KeyInfo> keyInfoCache = new HashMap<>();
 
     public static void setUp(CommandLine cmd) {
 
@@ -61,12 +61,45 @@ public class SecureSigningTool {
      */
     public static long loginSession(long slotID, byte[] userPIN, long flags, NativePointer application,
                                     CK_NOTIFY notify) {
-        long session = CE.OpenSession(slotID, flags, application, notify);
-        CE.LoginUser(session, userPIN);
-        return session;
+        try {
+            long session = CE.OpenSession(slotID, flags, application, notify);
+            CE.LoginUser(session, userPIN);
+            return session;
+        }catch (CKRException rv) {
+            throw new RuntimeException(rv);
+        }
     }
 
+    public static String getEcAlgo(CKA ecParams){
+        ASN1ObjectIdentifier oid = null;
+        String algo = null;
+
+        if(ecParams.getValueStr() != null && ecParams.getValueStr().equals("edwards25519")){
+            algo = "Ed25519";
+        }else if(ecParams.getValueStr() != null) {
+            try (ASN1InputStream stream = new ASN1InputStream(ecParams.getValue())) {
+                final ASN1Primitive primitive = stream.readObject();
+                if (primitive instanceof ASN1String) {
+                    final ASN1String string = (ASN1String) primitive;
+                    if ("curve25519".equalsIgnoreCase(string.getString())) {
+                        algo = "Ed25519";
+                    } else if ("Ed25519".equalsIgnoreCase(string.getString())) {
+                        algo = "Ed25519";
+                    } else if ("curve448".equalsIgnoreCase(string.getString())) {
+                        algo = "Ed448";
+                    }
+                } else {
+                    oid = ASN1ObjectIdentifier.getInstance(ecParams.getValue());
+                    algo = ECNamedCurveTable.getName(oid);
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return algo;
+    }
     public static void privateKeyList(){
+
         long session = loginSession(INITSLOT, USER_PIN,
                 CK_SESSION_INFO.CKF_RW_SESSION | CK_SESSION_INFO.CKF_SERIAL_SESSION, null, null);
 
@@ -74,64 +107,110 @@ public class SecureSigningTool {
                 new CKA(CKA.CLASS, CKO.PRIVATE_KEY),
         };
 
+
         long[] keyRefs = CE.FindObjects(session, templ);
+
+        Object[][] data = new Object[keyRefs.length][5];
+        int i = 0;
 
         for(long key : keyRefs){
             String label = CE.GetAttributeValue(session,key,CKA.LABEL).getValueStr();
             String id = CE.GetAttributeValue(session,key,CKA.ID).getValueStr();
             long key_type = CE.GetAttributeValue(session,key,CKA.KEY_TYPE).getValueLong();
             CKA ecParams = CE.GetAttributeValue(session,key,CKA.EC_PARAMS);
-            BigInteger modulus = CE.GetAttributeValue(session,key,CKA.MODULUS).getValueBigInt();
+            //BigInteger modulus = CE.GetAttributeValue(session,key,CKA.MODULUS).getValueBigInt();
             Long modulusBits = CE.GetAttributeValue(session,key,CKA.MODULUS_BITS).getValueLong();
-            BigInteger rsaExponent = CE.GetAttributeValue(session,key,CKA.PUBLIC_EXPONENT).getValueBigInt();
+            //BigInteger rsaExponent = CE.GetAttributeValue(session,key,CKA.PUBLIC_EXPONENT).getValueBigInt();
 
-            ASN1ObjectIdentifier oid = null;
-            String algo = null;
-            
-            if(ecParams.getValueStr() != null && ecParams.getValueStr().equals("edwards25519")){
-                algo = "Ed25519";
-            }else if(ecParams.getValueStr() != null) {
-                try (ASN1InputStream stream = new ASN1InputStream(ecParams.getValue())) {
-                    final ASN1Primitive primitive = stream.readObject();
-                    if (primitive instanceof ASN1String) {
-                        final ASN1String string = (ASN1String) primitive;
-                        if ("curve25519".equalsIgnoreCase(string.getString())) {
-                            algo = "Ed25519";
-                        } else if ("Ed25519".equalsIgnoreCase(string.getString())) {
-                            algo = "Ed25519";
-                        } else if ("curve448".equalsIgnoreCase(string.getString())) {
-                            algo = "Ed448";
-                        }
-                    } else {
-                        oid = ASN1ObjectIdentifier.getInstance(ecParams.getValue());
-                        algo = ECNamedCurveTable.getName(oid);
-                    }
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
+            String algo = getEcAlgo(ecParams);
+
+            if(label != null){
+                label = label.substring(0, label.length() - 8);
+            }else{
+                label = id;
             }
 
-            KeyInfo info = new KeyInfo(label, id, CKK.L2S(key_type), algo, modulus, modulusBits, rsaExponent);
+            /*
+            System.out.println("Key Ref: " + key);
+            System.out.println("Name: " + label);
+            //System.out.println("ID: " + id);
+            System.out.println("Key Type: " + CKK.L2S(key_type));
+            if(algo != null){
+                System.out.println("EC Params: " + algo);
+            }
+            //System.out.println("Key Size: " + modulusBits);
+            //System.out.println("RSA Modulus: " + modulus);
+            //System.out.println("RSA Public Exponent: " + rsaExponent);
+            //System.out.println("----------------------------------------------\n");
 
-            keyInfoCache.put(key,info);
+            */
+            data[i][0] = key;
+            data[i][1] = label;
+            data[i][2] = CKK.L2S(key_type);
+            data[i][3] = algo;
+            data[i][4] = modulusBits;
+            i = i+1;
         }
 
-        for(Map.Entry<Long, KeyInfo> entry : keyInfoCache.entrySet()){
-            System.out.println("Key Ref: " + entry.getKey());
-            System.out.println("Label: " + entry.getValue().label);
-            System.out.println("ID: " + entry.getValue().id);
-            System.out.println("Key Type: " + entry.getValue().keyType);
-            System.out.println("Key Size: " + entry.getValue().keySize);
-            System.out.println("EC Params: " + entry.getValue().ecParams);
-            System.out.println("RSA Modulus: " + entry.getValue().modulus);
-            System.out.println("RSA Public Exponent: " + entry.getValue().rsaExponent);
-            System.out.println("----------------------------------------------\n");
+        String[] columnNames = {"Key Ref", "Alias", "Key Type", "EC Params", "Key Size"};
+        TextTable tt = new TextTable(columnNames, data);
+        tt.printTable();
+    }
+
+    public static void sign(String key, String hashAlgo, String path){
+        long session = loginSession(INITSLOT, USER_PIN,
+                CK_SESSION_INFO.CKF_RW_SESSION | CK_SESSION_INFO.CKF_SERIAL_SESSION, null, null);
+        Long privateKey = Long.parseLong(key);
+
+        CKA ecParams = CE.GetAttributeValue(session,privateKey,CKA.EC_PARAMS);
+        String ecAlgo = getEcAlgo(ecParams);
+        System.out.println(key);
+        System.out.println(hashAlgo);
+        System.out.println(path);
+        String data = "";
+        try {
+            data = new String(Files.readAllBytes(Paths.get(path)));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.print("\n" + data.length() + "\n");
+
+        if(hashAlgo == null || hashAlgo.toUpperCase().equals("NONE") || ecAlgo.equals("Ed25519")){
+            log.info("Hash algorithm not specified, signing data");
+
+            CE.SignInit(session, new CKM(CKM.ECDSA), privateKey);
+            byte[] sig = CE.Sign(session, data.getBytes());
+
+            byte[] base64 = Base64.getEncoder().encode(sig);
+
+            System.out.println("Sig: " + Hex.b2s(sig));
+            System.out.println("Base 64: " + new String(base64));
+        }else{
+            log.info("Hashing with " + hashAlgo + " then signing data");
+            try {
+                MessageDigest digest = MessageDigest.getInstance(hashAlgo);
+                byte[] hash = digest.digest(data.getBytes(StandardCharsets.UTF_8));
+
+                CE.SignInit(session, new CKM(CKM.ECDSA), privateKey);
+                byte[] sig = CE.Sign(session, hash);
+
+                byte[] base64 = Base64.getEncoder().encode(sig);
+
+                System.out.println("Sig: " + Hex.b2s(sig));
+                System.out.println("Base 64: " + new String(base64));
+
+            } catch (NoSuchAlgorithmException e) {
+                throw new RuntimeException(e);
+            }
+            catch (CKRException rv) {
+                throw new RuntimeException(rv);
+            }
         }
     }
 
     public static void main (String[] args) throws ParseException {
         if(args[0].equals("version")){
-            System.out.println("stt 0.1");
+            System.out.println("sst 0.1");
         }else if(args[0].equals("listKeys")){
             Options options = new Options();
             options.addOption(HSM_SLOT);
@@ -143,105 +222,28 @@ public class SecureSigningTool {
             if(cmd.hasOption(HSM_SLOT.getOpt()) && cmd.hasOption(HSM_SLOT_PWD.getOpt()) && cmd.hasOption(P11_LIB.getOpt())){
                 setUp(cmd);
                 privateKeyList();
+            }else{
+                System.out.println("Missing arguments");
             }
 
         }else if(args[0].equals("sign")){
+            Options options = new Options();
+            options.addOption(HSM_SLOT);
+            options.addOption(HSM_SLOT_PWD);
+            options.addOption(P11_LIB);
+            options.addOption(KEY_REF);
+            options.addOption(HASH_ALGO);
+            options.addOption(PATH);
+            CommandLineParser parser = new DefaultParser();
+            CommandLine cmd = parser.parse( options, args);
 
-        }
-    /*
-        Options options = new Options();
-        // Commands
-        options.addOption("version", false, "Project Version");
-        options.addOption("listKeys", true, "Lists keys at specified slot.");
-        options.addOption("sign", true, "Create Signature using referenced key on specified slot.");
+            if(cmd.hasOption(HSM_SLOT.getOpt()) && cmd.hasOption(HSM_SLOT_PWD.getOpt()) && cmd.hasOption(P11_LIB.getOpt()) && cmd.hasOption(KEY_REF.getOpt()) && cmd.hasOption(PATH.getOpt())){
+                setUp(cmd);
+                sign(cmd.getOptionValue(KEY_REF.getOpt()), cmd.getOptionValue(HASH_ALGO.getOpt()), cmd.getOptionValue(PATH.getOpt()));
+            }else{
+                System.out.println("Missing arguments");
+            }
 
-        //Switches
-
-        CommandLineParser parser = new DefaultParser();
-        CommandLine cmd = parser.parse( options, args);
-
-        if(cmd.hasOption("version")){
-        }else if(cmd.hasOption("listKeys")){
-            Properties listkeys = cmd.getOptionProperties("listKeys");
-
-        }
-
-     */
-    }
-
-    static public class KeyInfo{
-        private String label;
-        private String id;
-        private String keyType;
-        private String ecParams;
-        private BigInteger modulus;
-        private Long keySize;
-        private BigInteger rsaExponent;
-
-        public KeyInfo(String label, String id, String keyType, String ecParams, BigInteger modulus, Long keySize, BigInteger rsaExponent) {
-            this.label = label;
-            this.id = id;
-            this.keyType = keyType;
-            this.ecParams = ecParams;
-            this.modulus = modulus;
-            this.keySize = keySize;
-            this.rsaExponent = rsaExponent;
-        }
-
-        public String getLabel() {
-            return label;
-        }
-
-        public void setLabel(String label) {
-            this.label = label;
-        }
-
-        public String getId() {
-            return id;
-        }
-
-        public void setId(String id) {
-            this.id = id;
-        }
-
-        public String getKeyType() {
-            return keyType;
-        }
-
-        public void setKeyType(String keyType) {
-            this.keyType = keyType;
-        }
-
-        public String getEcParams() {
-            return ecParams;
-        }
-
-        public void setEcParams(String ecParams) {
-            this.ecParams = ecParams;
-        }
-
-        public BigInteger getModulus() {
-            return modulus;
-        }
-
-        public void setModulus(BigInteger modulus) {
-            this.modulus = modulus;
-        }
-
-        public BigInteger getRsaExponent() {
-            return rsaExponent;
-        }
-
-        public void setRsaExponent(BigInteger rsaExponent) {
-            this.rsaExponent = rsaExponent;
-        }
-
-        public Long getKeySize() {
-            return keySize;
-        }
-
-        public void setKeySize(Long keySize) {
-            this.keySize = keySize;
         }
     }
 }
